24600161fdf9b66fd13866c522a7e0e9e019462b
mwalughabura <mwalughabura@gmail.com>
2018-03-06 15:45:08 +0300

    Add the methods   - Specified the methods that have been added to be POST

--- app/routes.py
+++ app/routes.py
@@ -5,21 +5,21 @@ def index():
     return "Hello, Mwalugha!"
 
 # login route
-@app.route('/api/auth/login')
+@app.route('/api/auth/login', methods = ['POST'])
 def login():
 	return "You have successfully logged in."
 
 # register route
-@app.route('/api/auth/register')
+@app.route('/api/auth/register', methods = ['POST'])
 def login():
 	return "You have successfully registered."
 
 # log out route
-@app.route('/api/auth/logout')
+@app.route('/api/auth/logout', methods = ['POST'])
 def login():
 	return "You have logged out."
 
 # reset password route
-@app.route('/api/auth/reset-password')
+@app.route('/api/auth/reset-password', methods = ['POST'])
 def login():
 	return "Your password has been reset."
